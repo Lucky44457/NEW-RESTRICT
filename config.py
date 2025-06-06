d64718b584313d08d6b84c8fed1df3dffcb104db
Lucky44457 <maribelwate@gmail.com>
2025-06-06 20:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://rex380895:sxz1J690QyA5RIaj@cluster0.scyfy8x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "6425525488").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "MANGO_DB")
-STRING = os.getenv("STRING", "1BVtsOJ4BuyBlTNK6O6tqteRyAGEXUYAlyb6lYkcMo0dIclJY6Lc5UK4XQZY7MrDjC11RTo-OpVEIQZvPxD2MJ7_2dlL75XN0U3D55nluEMFZ5UpCcA5IHdzCspk1M8ONckq0Zb4s6r-9MQm4-ml77uNM7fVv_MjUGxCBsMgqYXIypB6Co1M17yYtMPOST1vQFpa8YMSUeVBvP2eQJQ20pkdyqmLlsuAUbgO_YCO9WiVwLmGVsr-nIcMv6baxLnKljDBqDCTgfTxEzb9JCYAiwSHGkQ_f-zX-k04FZx8pzjaNFjO1zMU84TNleae8rC7ccsL5NNUhFV8jGMigT3T6zoi4_Qmx5f8=") # optional
+STRING = os.getenv("STRING", None) # optional
 LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002780402081")) # optional with -100
 FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002780402081")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "lq0a7uQHiGP00yzzYN3YF1yOIqIddCygNm9lv4rUM64=") # for session encryption
